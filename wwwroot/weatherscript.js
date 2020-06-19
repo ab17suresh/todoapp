214f63d44f0e874a94788cbe0b6ffe1de60fc4f8
ab17suresh <ab17suresh@gmail.com>
2020-06-19 17:20:20 +0530

    weather app1

--- wwwroot/weatherscript.js
+++ wwwroot/weatherscript.js
@@ -1,9 +1,12 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 
+=======
+>>>>>>> Stashed changes
 function submit1()
 {
     const input1 =document.getElementById("input1").value;
-    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+input1+'&appid=86793c0cec4fd5ff7dfdca2464bee84c';
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+input1+'&appid=86793c0cec4fd5ff7dfdca2464bee84c';   
     console.log(input1);
     fetch(url)
         .then(Response => Response.json())
@@ -11,9 +14,33 @@ function submit1()
         .then(data => {
             var cityname = data.name;
             var tempvalue = data.main.temp;
+            var climate = data.weather[0].main;
+            var iconcode = data.weather[0].icon;
+            const today = new Date();
+            const tomorrow = new Date(today);            
+            var url1 = 'https://api.openweathermap.org/data/2.5/forecast?q='+cityname+'&appid=86793c0cec4fd5ff7dfdca2464bee84c';
+            var url2 = 'https://pro.openweathermap.org/data/2.5/climate/month?q='+cityname+'&appid=86793c0cec4fd5ff7dfdca2464bee84c';
+            fetch(url1)
+                .then(Response => Response.json())
+                //.then(data1 => console.log(data1.list[9]))
+                .then(data1 => {
+                    var tomorrowtempvalue = data1.list[9].main.temp;
+                    var tomorrowclimate = data1.list[9].weather[0].main;
+                    var tommorrowiconcode = data1.list[9].weather[0].icon;
+                    document.getElementById("outputdate2").innerHTML =today.getDate()+1+"-"+today.getMonth()+"-"+today.getFullYear();
+                    document.getElementById("outputr2").innerHTML = Math.round(tomorrowtempvalue-273.15) + " &#8451;";
+                    document.getElementById("outputr3").innerHTML = tomorrowclimate;
+                    var iconurl1 = "http://openweathermap.org/img/wn/" + tommorrowiconcode + "@2x.png";
+                    document.getElementById("outputr4").innerHTML="<img src='"+iconurl1+"' id ='img1'/>";
+                    })
+            document.getElementById("outputdate1").innerHTML = today.getDate()+"-"+today.getMonth()+"-"+today.getFullYear();
             document.getElementById("output1").innerHTML = cityname;
-            document.getElementById("output2").innerHTML = tempvalue;
+            document.getElementById("output2").innerHTML = Math.round(tempvalue-273.15) + " &#8451;";
+            document.getElementById("output3").innerHTML = climate;           
+            var iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+            document.getElementById("output4").innerHTML="<img src='"+iconurl+"' id ='img1'/>";
             document.getElementById("input1").value = " ";
+<<<<<<< Updated upstream
         })
     
 
@@ -69,3 +96,10 @@ function submit1()
 
 }
 >>>>>>> 1ec1501f506c80d4edb48f988949b2d22718c073
+=======
+            fetch(url2)
+                .then(Response => Response.json())
+                .then(data2 => console.log(data2))
+    })
+}
+>>>>>>> Stashed changes
